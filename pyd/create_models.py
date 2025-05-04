2f68b206bad25131e8fe4e9e7950851605c5c9c7
Nikita <romashko.04@mail.ru>
2025-05-04 20:04:35 +0500

    feat(models): добавлены модели для создания сущностей (CreateUser, CreateAddress и др.)

--- pyd/create_models.py
+++ pyd/create_models.py
@@ -9,3 +9,32 @@ class CreateProduct(BaseModel):
 
 class CreateCategory(BaseModel):
     name:str = Field(example="ЕДА")
+
+class CreateUser(BaseModel):
+    name: str = Field(example="Иван")
+    last_name: str = Field(example="Иванов")
+    password_hash:str=Field(example="qweqwqe123")
+    patronymic: str = Field(example="Иванович")
+    email: str = Field(example="example@mail.ru")
+    role_id:int=Field(example=1)
+    phone: str = Field(example="+79123456789")
+
+class CreateRole(BaseModel):
+    name: str = Field(example="admin")
+
+class CreateAddress(BaseModel):
+    address: str = Field(example="г. Москва, ул. Ленина, д. 1")
+    user_id:int=Field(example=1)
+
+class CreateOrder(BaseModel):
+    total_price: float = Field(example=100.0)
+    created_at: str = Field(example="2023-10-01T12:00:00")
+
+class CreateOrderStatus(BaseModel):
+    name: str = Field(example="Выполнен")
+
+class CreateShop(BaseModel):
+    name: str = Field(example="Магазин 1")
+    description: str = Field(example="Магазин электроники")
+    custom_domain:str = Field(example="example")
+    logo_path: str = Field(example="path/to/logo.png")
