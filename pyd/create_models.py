b529895684a1896bc08d9e3bb2d88e07cb7675a3
Nikita <romashko.04@mail.ru>
2025-05-04 20:45:36 +0500

    fix(models): поля description, image_path в CreateProduct, patronymic в CreateUser и description, logo_path в CreateShop теперь не обязательны

--- pyd/create_models.py
+++ pyd/create_models.py
@@ -3,9 +3,10 @@ from pydantic import BaseModel, Field
 class CreateProduct(BaseModel):
     name: str = Field(min_length=3,max_length=255,example="Молоко")
     price: float = Field(example=10.85)
-    description: str= Field(example='Вкусное молоко')
-    image_path: str= Field(example='dsdasd')
+    description: str | None = Field(example='Вкусное молоко')
+    image_path: str | None = Field(example='dsdasd')
     category_id: int = Field(example=1)
+    shop_id:int=Field(example=1)
 
 class CreateCategory(BaseModel):
     name:str = Field(example="ЕДА")
@@ -14,7 +15,7 @@ class CreateUser(BaseModel):
     name: str = Field(example="Иван")
     last_name: str = Field(example="Иванов")
     password_hash:str=Field(example="qweqwqe123")
-    patronymic: str = Field(example="Иванович")
+    patronymic: str | None = Field(example="Иванович")
     email: str = Field(example="example@mail.ru")
     role_id:int=Field(example=1)
     phone: str = Field(example="+79123456789")
@@ -35,6 +36,6 @@ class CreateOrderStatus(BaseModel):
 
 class CreateShop(BaseModel):
     name: str = Field(example="Магазин 1")
-    description: str = Field(example="Магазин электроники")
+    description: str | None = Field(example="Магазин электроники")
     custom_domain:str = Field(example="example")
-    logo_path: str = Field(example="path/to/logo.png")
+    logo_path: str | None = Field(example="path/to/logo.png")
