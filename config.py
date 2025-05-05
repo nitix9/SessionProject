ff4d65ddd4a723cd9b9028b443c159e1bf3526e3
Nikita <romashko.04@mail.ru>
2025-05-05 11:34:06 +0500

    feat(product): реализована загрузка изображения продукта

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
 
 class Settings(BaseSettings):
     DB_NAME: str = "default"
@@ -6,7 +7,9 @@ class Settings(BaseSettings):
     DB_PASSWORD: str = "default"
     DB_HOST: str = "localhost"
     DB_PORT: int = 3306
-    
+    ALLOWED_CONTENT_TYPES: List[str]=['image/png','image/webp']
+    ALLOWED_EXTENSIONS: List[str]=['jpg']
+    MAX_FILE_SIZE:int=2097152
     model_config=SettingsConfigDict(env_file=".env")
 
 settings= Settings()
